8a4ce44ed972d19b2bc76f8aa16ea9c2d2e967ef
albamachuk <machuca.pacheco.alba@iescamas.es>
2025-09-30 19:16:17 +0200

    ejercicio10

---
+++ Actividad2/src/Unidad2/actividad1010.java
@@ -0,0 +1,12 @@
+package Unidad2;
+import java.util.Scanner;
+public class actividad1010 {
+	public static void main(String[] args) {
+	  Scanner scanner = new Scanner(System.in);
+	  
+	  System.out.print("Introduce un número de (2-4 cifras)");
+      int numero = scanner.nextInt();
+      
+      
+	}
+}
